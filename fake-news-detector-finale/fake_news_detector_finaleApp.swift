31988cd30fc6b4475edcf1f0e6993e546cc4411c
Max-Sapaniuk <maksym.sapaniuk@gmail.com>
2024-11-20 13:26:04 +0200

    Initial Commit

---
+++ fake-news-detector-finale/fake_news_detector_finaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fake_news_detector_finaleApp.swift
+//  fake-news-detector-finale
+//
+//  Created by Maksym Sapaniuk on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct fake_news_detector_finaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
